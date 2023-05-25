8b98187e018f200b9e84b99823cb61c4ab3c34e9
LoreSVi <loresuvivas@gmail.com>
2023-05-25 03:09:23 -0300

    estilos

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import { BsFillCartCheckFill } from "react-icons/bs";
+import "./navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div className nav>
-      <h1>DogStore Mila</h1>
+    <div className="nav">
+      <img
+        src="https://res.cloudinary.com/dtgaqvham/image/upload/v1684949095/LogoSample_ByTailorBrands_eft7pt.png"
+        alt="imagen logo"
+      />
       <ul>
         <li>Alimento y Snacks</li>
         <li>Higiene</li>
