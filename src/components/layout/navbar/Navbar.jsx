42bde7f83c7a262aba75543eb25480ada947ae3b
LoreSVi <loresuvivas@gmail.com>
2023-05-26 10:11:22 -0300

    cambio estilos navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -2,23 +2,25 @@ import { BsFillCartCheckFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div className="nav">
-     
-      <ul>
-      <li> <img
-        src="https://res.cloudinary.com/dtgaqvham/image/upload/v1684949095/LogoSample_ByTailorBrands_eft7pt.png"
+    <nav>
+       <div>
+       <ul>
+   <li> <img
+        src="https://res.cloudinary.com/dtgaqvham/image/upload/v1685105298/DogStoreMilaLogosm_bdlz7e.png"
         alt="imagen logo"
-      />   </li>
-        <li>Alimento y Snacks</li>
-        <li>Higiene</li>
-        <li>Juguetes</li>
-        <li>Pulguisidas</li>
-       <li> <BsFillCartCheckFill color="purple" size="23px" />
-      <span>1</span>
-    </li>
-      </ul>
-     
-    </div>
+      /></li>  
+          <li>Alimento y Snacks</li>
+          <li>Higiene</li>
+          <li>Juguetes</li>
+          <li>Pulguisidas</li>
+          <li>
+            {" "}
+            <BsFillCartCheckFill color="#b22222" size="23px" />
+            <span>1</span>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
