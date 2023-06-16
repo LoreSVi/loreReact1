070b0a3aef826e1981a9ddd1772d975ae12fb120
LoreSVi <loresuvivas@gmail.com>
2023-06-15 22:59:01 -0300

    cambio en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import Navbar from "./components/layout/navbar/Navbar.jsx";
-import { ItemListContainer } from "./components/pages/productsList/ProductsListContainer.jsx";
-import { useState } from "react";
+import Layout from "./components/layout/Layout";
 
+import CartContainer from "./components/pages/cart/CartContainer";
+import ProductDetailContainer from "./components/pages/productDetail/ProductDetailContainer";
+import ProductsListContainer from "./components/pages/productsList/ProductsListContainer";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  const [nombre, setNombre] = useState("usuario");
-  const [islogued, setIsLogued] = useState(true);
-
-  console.log(islogued);
-
-  let saludo = "Hola";
   return (
-    <div>
-      <Navbar />
-      <ItemListContainer nombre={nombre} saludo={saludo} />
-      <button onClick={() => setNombre("Lore")}>Cambiar nombre</button>
-      <button onClick={() => setIsLogued(!islogued)}>
-        cambiar a {islogued ? "desloguearse" : "loguearse"}
-      </button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<ProductsListContainer />} />
+
+          <Route path="/category/:categoryName" element={<ProductsListContainer />} />
+
+          <Route path="/itemDetail/:id" element={<ProductDetailContainer />} />
+          
+          <Route path="/carrito" element={<CartContainer />} />
+        </Route>
+
+        <Route path="*" element={<h1>404 not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
+{
+  /* <div>
+      <Navbar>
+        <ProductsListContainer />
+        <ProductDetailContainer />
+      </Navbar>
+    </div> */
+}
