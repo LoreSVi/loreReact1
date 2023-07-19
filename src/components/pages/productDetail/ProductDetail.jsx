a80bd551497378d0d82a73f7387ace88aa5f3ab3
LoreSVi <loresuvivas@gmail.com>
2023-07-19 08:09:16 -0300

    cambios product detail

--- src/components/pages/productDetail/ProductDetail.jsx
+++ src/components/pages/productDetail/ProductDetail.jsx
@@ -1,7 +1,6 @@
 import { ItemCount } from "../../common/ItemCount";
 import "./ProductDetail.css";
 
-
 const ProductDetail = ({ productSelected, cantidad, onAdd }) => {
   return (
     <>
@@ -9,14 +8,22 @@ const ProductDetail = ({ productSelected, cantidad, onAdd }) => {
         <div className="item-card">
         <h2>{productSelected.title}</h2>
         <img src={productSelected.img} alt="" />
+        <h4>{productSelected.price}</h4>
+        <h4>{productSelected.description}</h4>
      </div>
-    
-      <ItemCount
-        stock={productSelected.stock}
-        initial={cantidad}
-        onAdd={onAdd}
-        />
-      </div>
+      {productSelected.stock > 0 ? (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <ItemCount
+            stock={productSelected.stock}
+            onAdd={onAdd}
+            initial={cantidad}
+          />
+        </div>
+      ) : (
+        <h2>No hay stock</h2>
+      )}
+
+    </div>
     </>
   );
 };
